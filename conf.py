e2b6e0c737dc31c71861ce20cd28460529c13809
Sami Juvonen <sami@juvonen.org>
2016-08-19 10:52:00 -0700

    Nikola auto commit.
    Nikola version: 7.7.11

--- conf.py
+++ conf.py
@@ -780,7 +780,7 @@ IMAGE_FOLDERS = {'images': 'images'}
 # vs
 # xcode
 # This list MAY be incomplete since pygments adds styles every now and then.
-CODE_COLOR_SCHEME = 'paraiso-light'
+CODE_COLOR_SCHEME = 'emacs'
 
 # If you use 'site-reveal' theme you can select several subthemes
 # THEME_REVEAL_CONFIG_SUBTHEME = 'sky'
